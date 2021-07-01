e021b928f1f4da6376d62408a53ea5475596b93b
dorpvom <dorp@cs.uni-bonn.de>
2021-07-01 12:51:15 +0200

    upgraded node version to fix installation issue

--- blackbox-opcua-fuzzing/targets/node-opc/install.sh
+++ blackbox-opcua-fuzzing/targets/node-opc/install.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get install -y npm
 
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
 
 npm install node-opcua --save
